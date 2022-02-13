022b2ec211f9ee5ee0425797932ee1760b9289a2
Andrii Lypovetskyi <li_andr@ukr.net>
2022-02-13 21:47:39 +0200

    Delete category

--- routes/category.js
+++ routes/category.js
@@ -32,11 +32,32 @@ router.post('/', async (req, res, next) => {
 
     const newCategory = await Category.create({ ...req.body });
     res.status(201).json(newCategory);
-  } catch (err) {
-    if (err.message.includes('Validation failed')) {
-      err.status = 404;
+  } catch (error) {
+    if (error.message.includes('Validation failed')) {
+      error.status = 404;
     }
-    next(err);
+    next(error);
+  }
+});
+
+// Удалить категорию по id.
+router.delete('/:categoryId', async (req, res, next) => {
+  const { categoryId } = req.params;
+  console.log(categoryId);
+  try {
+    const deleteCategory = await Category.findOneAndRemove({
+      _id: categoryId,
+    });
+
+    if (!deleteCategory) {
+      const error = new Error('Not found');
+      error.status = 404;
+      throw error;
+    }
+
+    res.json({ message: 'Category deleted' });
+  } catch (error) {
+    next(error);
   }
 });
 
