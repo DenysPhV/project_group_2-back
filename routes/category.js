375fd7cd4c178948f03053355edcd53e47e6760c
Andrii Lypovetskyi <li_andr@ukr.net>
2022-02-13 18:18:50 +0200

    Categories
    Get all categories and in POST verification of duplicates Categories

--- routes/category.js
+++ routes/category.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const { Conflict } = require('http-errors');
 
 const { Category } = require('../models');
 const { joiSchema } = require('../models/category');
+
+// Получить все категории
+router.get('/', async (req, res, next) => {
+  try {
+    const categories = await Category.find();
+    res.json(categories);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // Добавление новой категории
 router.post('/', async (req, res, next) => {
@@ -12,11 +23,17 @@ router.post('/', async (req, res, next) => {
       error.status = 400;
       throw error;
     }
+    // Проверка на дубль категории
+    const { nameCategory } = req.body;
+    const tempCategory = await Category.findOne({ nameCategory });
+    if (tempCategory) {
+      throw new Conflict('This category already exists');
+    }
 
     const newCategory = await Category.create({ ...req.body });
     res.status(201).json(newCategory);
   } catch (err) {
-    if (err.message.includes('validation failed')) {
+    if (err.message.includes('Validation failed')) {
       err.status = 404;
     }
     next(err);
@@ -24,10 +41,3 @@ router.post('/', async (req, res, next) => {
 });
 
 module.exports = router;
-
-// {
-//   "nameCategory": "Разное",
-//   "_id": "62091748b71f06d6de2cfcf8",
-//   "createdAt": "2022-02-13T14:35:52.388Z",
-//   "updatedAt": "2022-02-13T14:35:52.388Z"
-// }
