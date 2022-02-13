52834f31bb4c08566c04d91d216f616247d189a9
DmytroMS <78643116+DmytroMS@users.noreply.github.com>
2022-02-13 18:56:58 +0200

    auth2

--- routes/users.js
+++ routes/users.js
@@ -11,10 +11,42 @@ router.post('/signup', async(req, res, next) => {
 })
 
 
+<<<<<<< Updated upstream
 // login 
 
 router.post("/login", async(req, res, next) => {
    
+=======
+router.post('/login', async (req, res, next) => {
+  try {  
+    const {error} = joiLoginSchema.validate(req.body); 
+    if(error) {
+        throw new BadRequest(error.message);
+}
+const {email, password} = req.body;
+const user = await User.findOne({email});
+if(!user){
+    throw new Unauthorized("Email or password is wrong")
+}
+const passwordCompare = await bcrypt.compare(password, user.password);
+if(!passwordCompare){
+    throw new Unauthorized("Email or password is wrong")
+}
+    const {_id} = user;
+    const payload = {id: _id};
+    const token = jwt.sign(payload, SECRET_KEY, {expiresIn: '2h'});
+    await User.findByIdAndUpdate(_id, {token});
+    res.json({
+        token,
+        user: {
+         email}
+    })
+
+ } catch (error) {
+    next(error);
+}
+
+>>>>>>> Stashed changes
 });
 
 module.exports = router;
