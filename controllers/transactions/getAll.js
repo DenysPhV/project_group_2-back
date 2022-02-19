cbdc6c7ad3bc38dec84a74914d4f8262c3f4c4de
Vlad <66143346+kvlad0610@users.noreply.github.com>
2022-02-19 23:37:00 +0200

    Добавил nameCategory при получении всех транзакций

--- controllers/transactions/getAll.js
+++ controllers/transactions/getAll.js
@@ -1,4 +1,4 @@
-const { Transaction } = require('../../models');
+const { Transaction, Category } = require('../../models');
 
 const getAll = async (req, res, next) => {
   try {
@@ -8,7 +8,24 @@ const getAll = async (req, res, next) => {
       { owner: _id },
       '-createdAt -updatedAt',
     );
-    res.json(transactions);
+
+    const categories = await Category.find();
+
+    const newTransactions = [];
+
+    for (let transaction of transactions) {
+      for (const category of categories) {
+        if (String(category._id) === String(transaction.categoryId)) {
+          transaction = {
+            ...transaction._doc,
+            nameCategory: category.nameCategory,
+          };
+          break;
+        }
+      }
+      newTransactions.push(transaction);
+    }
+    res.json(newTransactions);
   } catch (error) {
     next(error);
   }
