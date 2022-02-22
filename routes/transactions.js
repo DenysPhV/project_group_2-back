83a2cd9cb25211388792708c1ef5c305e01479c3
Vlad <66143346+kvlad0610@users.noreply.github.com>
2022-02-23 00:11:29 +0200

    Добавление описаний функций

--- routes/transactions.js
+++ routes/transactions.js
@@ -5,8 +5,13 @@ const { add, getAll, getStatistics } = require('../controllers/transactions');
 
 const router = express.Router();
 
+// Получить все транзакции
 router.get('/', authenticate, getAll);
+
+// Получить транзакции отфильтрованы по месяцу и году
 router.get('/statistics', authenticate, getStatistics);
+
+// Добавить транзакцию
 router.post('/', authenticate, add);
 
 module.exports = router;
