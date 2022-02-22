bf3f90e56da5400787b4b10ebb0edc6a8cecda09
Vlad <66143346+kvlad0610@users.noreply.github.com>
2022-02-23 00:12:26 +0200

    Удаление лишнего кода

--- models/transaction.js
+++ models/transaction.js
@@ -55,7 +55,6 @@ const joiSchemaTransaction = Joi.object({
   sum: Joi.number().required(),
   balance: Joi.number(),
   comment: Joi.string(),
-  // categoryId: Joi.string().required(),
   nameCategory: Joi.string(),
 });
 
